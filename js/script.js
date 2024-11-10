28308f13576abeca34b34388f531ed862599c7d0
ktagdiwala <krishna.tagdiwala@gmail.com>
2024-11-10 00:51:51 -0800

    Completed form validation and functionality requirements

--- js/script.js
+++ js/script.js
@@ -2,8 +2,27 @@
 document.querySelector("#zip").addEventListener("change",displayCity);
 document.querySelector("#state").addEventListener("change",displayCounties);
 document.querySelector("#username").addEventListener("change",checkUsername);
+document.querySelector("#password").addEventListener("focus",suggestPassword);
+document.querySelector("#signupForm").addEventListener("submit",function(event){
+    validateForm(event);
+});
+// Wrapped in anonymous function to ensure that displayStates() is executed once the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", () => displayStates());
 
 // functions
+
+// Displaying the list of US states from Web API
+async function displayStates(){
+    // console.log("The page has loaded...");
+    let url = `https://csumb.space/api/allStatesAPI.php`;
+    let response = await fetch(url);
+    let data = await response.json();
+    // console.log(data);
+    let stateList = document.querySelector("#state");
+    for(let i = 0; i<data.length; i++){
+        stateList.innerHTML += `<option value="${data[i].usps}">${data[i].state}</option>`;
+    }
+}
 
 // Displaying city from Web API after entering zip code
 async function displayCity() {
@@ -12,13 +31,19 @@ async function displayCity() {
     let url = `https://csumb.space/api/cityInfoAPI.php?zip=${zipCode}`;
     let response = await fetch(url);
     let data = await response.json();
+    let zipError = document.querySelector("#zipError");
     // console.log(data);
-    document.querySelector("#city").innerHTML = data.city;
-    document.querySelector("#latitude").innerHTML = data.latitude;
-    document.querySelector("#longitude").innerHTML = data.longitude;
-    // document.querySelector("#state").innerHTML = data.state;
-    // document.querySelector("#county").innerHTML = data.county;
-    
+    if(data==false){
+        zipError.innerHTML = "This zipcode does not exist";
+        zipError.style.color = "red";
+    }else{
+        zipError.innerHTML = "";
+        document.querySelector("#city").innerHTML = data.city;
+        document.querySelector("#latitude").innerHTML = data.latitude;
+        document.querySelector("#longitude").innerHTML = data.longitude;
+        // document.querySelector("#state").innerHTML = data.state;
+        // document.querySelector("#county").innerHTML = data.county;
+    }
 }
 
 async function displayCounties(){
@@ -47,4 +72,44 @@ async function checkUsername(){
         usernameError.innerHTML = "Username not available!";
         usernameError.style.color = "red";
     }
+}
+
+// Validating form data
+function validateForm(e){
+    let isValid = true;
+    let username = document.querySelector("#username").value;
+    if(username.length==0){
+        document.querySelector("#usernameError").innerHTML = "Username Required!";
+        document.querySelector("#usernameError").style.color = "red";
+        isValid = false;
+    }
+    let password = document.querySelector("#password").value;
+    let confirmPassword = document.querySelector("#confirmPassword").value;
+    let passError = document.querySelector("#passwordError");
+
+    if(password.length<6){
+        passError.innerHTML = "Password must be at least 6 characters";
+        passError.style.color = "red";
+        isValid = false;
+    }else if(password!=confirmPassword){
+        passError.innerHTML = "Passwords must match";
+        passError.style.color = "red";
+        isValid = false;
+    }else{
+        document.querySelector("#passwordError").innerHTML = "";
+    }
+
+
+    if(!isValid){
+        e.preventDefault();
+    }
+}
+
+async function suggestPassword(){
+    let suggestPass = document.querySelector("#suggestPass");
+    let url = `https://csumb.space/api/suggestedPassword.php?length=8`;
+    let response = await fetch(url);
+    let data = await response.json();
+    // console.log(data.password);
+    suggestPass.innerHTML=data.password;
 }
